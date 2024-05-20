4fe82f5c76eb639170f1ace0e7569eb70a74b9c0
javierlopezfernandez <javisan81@hotmail.com>
2024-05-20 15:57:13 +0200

    Create Employees Supervisors class

---
+++ src/main/java/com/hierarchy/domain/EmployeesSupervisors.java
@@ -0,0 +1,11 @@
+package com.hierarchy.domain;
+
+import java.util.Map;
+
+public class EmployeesSupervisors {
+    private Map<String, String> employeeSupervisors;
+
+    public EmployeesSupervisors(Map<String, String> employeeSupervisors) {
+        this.employeeSupervisors = employeeSupervisors;
+    }
+}
