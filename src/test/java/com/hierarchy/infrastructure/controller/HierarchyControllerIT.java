35c142c4cfc80bdb75879a36b97eb610d5197a25
javierlopezfernandez <javisan81@hotmail.com>
2024-05-20 16:18:40 +0200

    Mock the hierarchyUseCase in the test

--- src/test/java/com/hierarchy/infrastructure/controller/HierarchyControllerIT.java
+++ src/test/java/com/hierarchy/infrastructure/controller/HierarchyControllerIT.java
@@ -1,11 +1,13 @@
 package com.hierarchy.infrastructure.controller;
 
 import com.hierarchy.Application;
+import com.hierarchy.services.HierarchyUseCase;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +20,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 public class HierarchyControllerIT {
     @Autowired
     private MockMvc mockMvc;
+    @MockBean
+    private HierarchyUseCase hierarchyUseCase;
 
     @Test
     public void emptyHierarchy() throws Exception {
